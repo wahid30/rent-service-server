918043bc35961ba857b1a26d36051f8313ecef7d
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-09 19:42:03 +0600

    deploy of node added

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const port = 5000;
 
 // middle wares
 app.use(cors());
+app.use(express.json());
+
+const myServices = require("./services.json");
+
+app.get("/services", (req, res) => {
+  res.send(myServices);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
