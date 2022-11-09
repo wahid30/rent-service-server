608ad52c21261e28d5f388313b6ac72a0948c24e
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-09 23:47:59 +0600

    post added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 var cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -22,11 +22,28 @@ async function run() {
   try {
     const serviceCollection = client.db("rentServices").collection("services");
 
+    const userReview = client.db("rentServices").collection("userReviews");
+
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // review api
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      // console.log("this is", review);
+      const result = await userReview.insertOne(review);
+      res.send(result);
     });
   } finally {
   }
