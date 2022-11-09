c6f4ebfbe9ccadf65df6af1213363d43021c300b
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-09 20:19:19 +0600

    mongo added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 var cors = require("cors");
 
 const port = 5000;
@@ -7,6 +9,21 @@ const port = 5000;
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gufvi8h.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 const myServices = require("./services.json");
 
